deaaf2760afe8bc38008c479146b3467fb92edc7
bhuwanhike <bhuwanhike@gmail.com>
2025-07-31 12:01:21 +0530

    updated frontend

--- Frontend/src/Routes/Investors.jsx
+++ Frontend/src/Routes/Investors.jsx
@@ -295,6 +295,7 @@ const SubmitInvestorForm = ({ onClose, addInvestor }) => {
     // startupData.fundingNeeded = Number(startupData.fundingNeeded);
     addInvestor(startupData); // Pass form data to the addStartup function
     onClose();
+    window.location.href = "/investors"; // Redirect to investors page after submission
   };
 
   const industryOptions = [
--- Frontend/src/Routes/Startup.jsx
+++ Frontend/src/Routes/Startup.jsx
@@ -245,6 +245,7 @@ const SubmitStartupForm = ({ onClose, addStartup }) => {
     // startupData.fundingNeeded = Number(startupData.fundingNeeded);
     addStartup(startupData); // Pass form data to the addStartup function
     onClose();
+    window.location.href = "/startups"; // Close the form on submit
   };
 
   const industryOptions = [
